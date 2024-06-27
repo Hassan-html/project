eef38f82ae7ae2b876bfdbbff5962cfa39a13d38
HSN-CODES <logics.nook@gmail.com>
2024-06-27 10:28:55 +0500

    "fixing"

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import EventCard from "@/app/components/EventCard";
+import { Card } from "flowbite-react";
 export default function Home() {
   const [data, setData] = useState([]);
   useEffect(() => {
@@ -21,7 +22,9 @@ export default function Home() {
     <>
       <h1 className="text-4xl">Below is the data</h1>
       <div className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold mb-6">Upcoming Events</h1>
+        <h1 className="text-3xl font-bold mb-6 text-blue-700">
+          Upcoming Events
+        </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {data.map((event) => (
             <EventCard key={event.id} event={event} />
